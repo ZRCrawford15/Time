d56af91c88f07014321dbbd0142aea12a45b6e59
ZRCrawford15 <48399752+ZRCrawford15@users.noreply.github.com>
2021-05-11 21:43:28 -0700

    Add files via upload

--- time.js
+++ time.js
@@ -5,7 +5,7 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 7479);
 
 
 function genContext(){
@@ -14,8 +14,20 @@ function genContext(){
   return stuffToDisplay;
 }
 
-app.get('/time',function(req,res){
+app.get('/',function(req,res){
   res.render('time', genContext());
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
 });
 
 
